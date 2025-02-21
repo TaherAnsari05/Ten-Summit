30546764e1ea1ff9952762c26040c401174e5eee
aashnajoshi <aashnahetal@gmail.com>
2025-02-21 18:51:27 +0530

    .

--- Backend/summit_app/models.py
+++ Backend/summit_app/models.py
@@ -36,6 +36,8 @@ class CommentForm(forms.ModelForm):
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    class Meta:
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.name
@@ -44,14 +46,13 @@ class Job(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='jobs')
     role = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
-    seniority = models.CharField(max_length=255, blank=True, null=True)  # Now optional
+    seniority = models.CharField(max_length=255, blank=True, null=True)
     job_type = models.CharField(max_length=255)
-    link = models.URLField(blank=True, null=True)  # Optional job link
+    link = models.URLField(blank=True, null=True)
     about_the_position = models.TextField(max_length=1000)
     responsibilities = models.TextField(max_length=1000)
     requirements = models.TextField(max_length=1000)
     nice_to_have = models.TextField(max_length=1000)
-
 
     def __str__(self):
         return self.role
