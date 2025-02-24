b3280583672fa7d0e0acb08c527c43632ce12241
aashnajoshi <aashnahetal@gmail.com>
2025-02-24 10:31:47 +0530

    .

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('schedule/', ScheduleView.as_view(), name='schedule_view'),
     path('speakers/', SpeakerView.as_view(), name='speaker_view'),
     path('speakers/<int:pk>/', SpeakerView.as_view(), name='speaker_view'),
-    path('comments/' , CommentView.as_view()),
-    path('agenda/', AgendaView.as_view())
+    path('comments/' , CommentView.as_view(), name='comment_view'),
+    path('agenda/', AgendaView.as_view(), name='agenda_view'),
 ]
