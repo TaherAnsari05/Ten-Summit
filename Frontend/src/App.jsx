75393c865ef1c26e4cf5204f4f9a5f9f6024ebf0
Chandrabhan Prakash Thakkangar <148081499+cthakkangar8894@users.noreply.github.com>
2025-02-25 18:09:19 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
-
+import React from "react";
+import TrustCenter from "./components/TrustCenter";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <TrustCenter />
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
+
